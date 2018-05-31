91adc3c23794a59fc9123c2437c535a91b710fb9
cebwhiteman <35513634+cebwhiteman@users.noreply.github.com>
2018-05-31 00:26:14 -0600

    Add files via upload

--- CircleStats.java
+++ CircleStats.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
 public class CircleStats {
+    
+    //public final double PI = 3.141592;
+    
     public static void main(String[] args) {
-    Scanner input=new Scanner(System.in);
-    double radius=input.nextDouble();
-    System.out.print("The circumference is:");
-    System.out.println(2*3.14*radius);
-    System.out.print("The area is:");
-    System.out.println(3.14*radius*radius);
+        double radius = 5.0;
+        System.out.print("The circumference is: ");
+        System.out.println(2*Math.PI*radius);
+        System.out.print("The area is: ");
+        System.out.println(Math.PI*radius*radius);
     }
+    
 }
